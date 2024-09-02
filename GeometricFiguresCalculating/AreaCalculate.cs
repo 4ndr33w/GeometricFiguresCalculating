c463715329f3508d20fc91605fb6a9f184c4b5b6
Andrew Muhlynin <rx.081@hotmail.com>
2024-09-02 19:35:41 +0500

    Добавьте файлы проекта.

---
+++ GeometricFiguresCalculating/AreaCalculate.cs
@@ -0,0 +1,95 @@
+﻿namespace GeometricFiguresCalculating
+{
+    public class AreaCalculate
+    {
+        public double CircleArea(double radius)
+        {
+            radius = radius > 0 ? radius : radius * (-1); //площадь не может быть отрицательной
+            double result = 2 * Math.PI * radius; 
+
+            return result;
+        }
+
+        public double TriangleArea (double sideA, double sideB, double sideC)
+        {
+            double result = 0;
+
+            double a = sideA > 0 ? sideA : (-1) * sideA;
+            double b = sideB > 0 ? sideB : (-1) * sideC;
+            double c = sideC > 0 ? sideB : (-1) * sideC;
+
+            double perimeter = a + b + c;
+            double semiPerimeter = perimeter / 2;
+
+            double p = semiPerimeter;
+
+            result = Math.Sqrt(p * (p-a) * (p-b) * (p-c));
+
+            return result;
+        }
+
+        public bool IsTriangleRectangular (double sideA, double sideB, double sideC)
+        {
+            bool result = false;
+
+            if (Math.Pow(sideC, 2) == Math.Pow(sideA, 2) + Math.Pow(sideB, 2) ||
+                Math.Pow(sideB, 2) == Math.Pow(sideA, 2) + Math.Pow(sideC, 2) ||
+                Math.Pow(sideA, 2) == Math.Pow(sideC, 2) + Math.Pow(sideB, 2))
+            {
+                result = true;
+            }
+
+            return result;
+
+        }
+
+        public double RectangleArea (double sideA, double sideB)
+        {
+            double _sideA = sideA > 0? sideA : -1 * sideA;
+            double _sideB = sideB > 0 ? sideB : -1 * sideB;
+
+            double result = _sideA * _sideB;
+
+            return result;
+        }
+
+        public double IrregularQuadrilateralArea (double sideA, double sideB, double sideC, double sideD)
+        {
+            double result = 0;
+
+            double a = sideA > 0? sideA : -1 * sideA;
+            double b = sideB > 0? sideB : -1 * sideB;
+            double c = sideC > 0? sideC : -1 * sideC;
+            double d = sideD > 0? sideD : -1 * sideD;
+
+            double perimeter = a + b + c + d;
+
+            double p = perimeter / 2;
+
+            result = Math.Sqrt( (p-a) * (p-b) * (p-c) * (p-d) );
+
+            return result;
+        }
+
+
+        public double UnknownFigureArea (double circleRadius)
+        {
+            return CircleArea(circleRadius);
+        }
+
+        public double UnknownFigureArea (double triangleSideA, double triangleSideB, double triangleSideC)
+        {
+            return TriangleArea(triangleSideA, triangleSideB, triangleSideC);
+        }
+
+        public double UnknownFigureArea(double rectanglelSideA, double rectangleSideB)
+        {
+            return RectangleArea(rectanglelSideA, rectangleSideB);
+        }
+
+        public double UnknownFigureArea(double IrrQuadrSideA, double IrrQuadrSideB, double IrrQuadrSideC, double IrrQuadrSideD)
+        {
+            return IrregularQuadrilateralArea(IrrQuadrSideA, IrrQuadrSideB, IrrQuadrSideC, IrrQuadrSideD);
+        }
+    }
+}
---
+++ GeometricFiguresCalculatingTests/AreaCalculateTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GeometricFiguresCalculating;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricFiguresCalculating.Tests
+{
+    [TestClass()]
+    public class AreaCalculateTests
+    {
+        [TestMethod()]
+        public void CircleAreaTest()
+        {
+            double radius = 10;
+            Assert.Fail();
+        }
+    }
+}
